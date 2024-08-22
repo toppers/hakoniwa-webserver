07cabde1c13ba01dddcab9a0e37d47f0e8973c81
Takashi Mori <kanetugu2015@gmail.com>
2024-08-23 08:18:28 +0900

    bug fixed

--- server/core/websocket_server.py
+++ server/core/websocket_server.py
@@ -1,13 +1,18 @@
 import asyncio
 from core.hako_web_server import HakoWebSocketServer
 
-def get_websocket_server():
-    websocket_server = HakoWebSocketServer()
+# グローバルなWebSocketサーバーインスタンス
+_websocket_server = None
 
-    # 新しいイベントループを作成し、スレッドに設定
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    return websocket_server
+def get_websocket_server():
+    global _websocket_server
+    if _websocket_server is None:
+        _websocket_server = HakoWebSocketServer()
+
+        # 新しいイベントループを作成し、スレッドに設定
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    return _websocket_server
 
 def run_websocket_server():
     websocket_server = get_websocket_server()
